20d7d5e279fc787e6a896220d3987e712aab8aab
kyleuphus <kyleuphus@gmail.com>
2025-10-03 13:14:17 -0400

    Initial commit

---
+++ test.java
@@ -0,0 +1,15 @@
+public class test {
+
+    public static void main(String[] args){
+
+        int n = 1;
+        while (n <13)
+        {
+            n = n + 3;
+            System.out.print(n + " ");
+
+        }
+    }
+
+
+}
